cfa4001d8540fdd080952ee6488dd6eb4b822984
MOSTAFA17RASHEEDY <mostafarasheedy17@gmail.com>
2026-05-20 21:40:09 +0300

    test: standalone function without backend import

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,3 @@
-import app from '../backend/src/index'
-
-export default app
+export default async function handler(req: any, res: any) {
+  res.status(200).json({ status: 'ok', method: req.method, path: req.url })
+}
